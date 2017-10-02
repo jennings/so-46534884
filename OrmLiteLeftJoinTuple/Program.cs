b013305e854ff9c574176a4f3711047d1812724f
Stephen Jennings <stephen.g.jennings@gmail.com>
2017-10-02 15:49:41 -0700

    Initial commit

---
+++ OrmLiteLeftJoinTuple/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using ServiceStack.OrmLite;
+using System;
+
+namespace OrmLiteLeftJoinTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new OrmLiteConnectionFactory(":memory:", SqliteDialect.Provider);
+            using (var db = factory.OpenDbConnection())
+            {
+                // Populate database
+                db.CreateTable<Order>();
+                db.CreateTable<LineItem>();
+                db.ExecuteSql(@"INSERT INTO ""Order"" (Name) VALUES ('Alpha')");
+
+                var query = db.From<Order>()
+                              .LeftJoin<LineItem>()
+                              .Where(o => o.OrderId == 1);
+
+                // This line returns a tuple with Item2 populated with "new LineItem()"
+                // I expected it to be null, since there is no record in the LineItem table
+                var results = db.SelectMulti<Order, LineItem>(query);
+
+                Console.WriteLine(JsonConvert.SerializeObject(results, Formatting.Indented));
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    class Order
+    {
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+        public override string ToString() => $"OrderId = {OrderId}, Name = {Name}";
+    }
+
+    class LineItem
+    {
+        public int LineItemId { get; set; }
+        public int OrderId { get; set; }
+        public override string ToString() => $"LineItemId = {LineItemId}, OrderId = {OrderId}";
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# OrmLite LeftJoin Tuple example
+
+This is a reproduction for [Stack Overflow question #TBD][so]
+
+[so]: https://stackoverflow.com/q/TBD
